ee77577661f10e38f3cb48244a7abc0893794295
simont207 <simontierney@yahoo.com>
2026-06-05 00:53:33 -0400

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -25,7 +25,7 @@ self.addEventListener('notificationclick', event => {
     })
   );
 });
-const CACHE_NAME = 'mlb-picks-v7';
+const CACHE_NAME = 'mlb-picks-v8';
 
 // Assets to pre-cache (offline shell)
 const PRECACHE = [
