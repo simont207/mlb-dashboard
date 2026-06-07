2136134f5efb58529ac5d8d108b76e111da4492c
simont207 <simontierney@yahoo.com>
2026-06-07 00:38:03 -0400

    fix: route tomorrow pitchers through server proxy

--- static/sw.js
+++ static/sw.js
@@ -25,7 +25,7 @@ self.addEventListener('notificationclick', event => {
     })
   );
 });
-const CACHE_NAME = 'mlb-picks-v13';
+const CACHE_NAME = 'mlb-picks-v14';
 
 // Assets to pre-cache (offline shell)
 const PRECACHE = [
